493475ef736332f153aba20c21a2736d474ca02e
Jure Rotar <jrotar0@gmail.com>
2021-02-01 01:02:57 +0100

    Added tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 module.exports = {
     purge: [],
-    darkMode: false, // or 'media' or 'class'
+    darkMode: 'class',
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                'sans': ['Montserrat', 'sans-serif']
+            },
+            borderWidth: {
+                'default': '1px'
+            },
+            maxWidth: {
+                'nav': 'calc(100% - 15rem)'
+            },
+            backgroundColor: {
+                'dark-main': '#18191A',
+                'dark-nav': '#242526',
+                'green-default': '#9DC02E',
+                'green-default-darker': '#8dac29'
+            }
+        },
     },
     variants: {
         extend: {},
