6cdfa152460d0efc1b9b8c4c8646b7d40f6572d1
Jure Rotar <jrotar0@gmail.com>
2021-02-02 12:24:17 +0100

    Added municipalities relationship

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -3,17 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Region extends Model
 {
 
     protected $hidden = [
-        'id'
     ];
 
     public function location(): HasOne
     {
         return $this->hasOne(Location::class);
+    }
+
+    public function municipalities(): HasMany
+    {
+        return $this->hasMany(Municipality::class);
     }
 }
