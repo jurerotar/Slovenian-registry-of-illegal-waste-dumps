ee2f5f22945730d9d4b1e3b309ba3b51fa4d7561
Jure Rotar <jrotar0@gmail.com>
2021-02-11 23:03:49 +0100

    Translated paths
    Signed-off-by: Jure Rotar <jrotar0@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'cookies'], function () {
     Route::get('/', [PageDataController::class, 'home'])->name('home');
-    Route::get('/export', [PageDataController::class, 'export'])->name('export');
-    Route::get('/report', [PageDataController::class, 'report'])->name('report');
-    Route::get('/region/{id}', [PageDataController::class, 'region'])->name('region');
-    Route::get('/municipality/{id}', [PageDataController::class, 'municipality'])->name('municipality');
+    Route::get('/zemljevid', [PageDataController::class, 'map'])->name('map');
+    Route::get('/izvoz', [PageDataController::class, 'export'])->name('export');
+    Route::get('/prijava', [PageDataController::class, 'report'])->name('report');
+    Route::get('/regija/{regions:slug}', [PageDataController::class, 'region'])->name('region');
+    Route::get('/obcina/{municipalities:slug}', [PageDataController::class, 'municipality'])->name('municipality');
 });
 
 Route::get('/test', [TestController::class, 'index']);
-Route::get('/download', [ExportController::class, 'export']);
+Route::get('/prenos', [ExportController::class, 'export']);
