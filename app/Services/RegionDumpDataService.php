fce0805ca4ab41ae153a34c92649ea455eb90d90
Jure Rotar <jrotar0@gmail.com>
2021-02-23 12:56:19 +0100

    Created hydration for interactive region map
    Signed-off-by: Jure Rotar <jrotar0@gmail.com>

---
+++ app/Services/RegionDumpDataService.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Models\Dump;
+
+class RegionDumpDataService
+{
+    private CacheService $cache;
+
+    public function __construct()
+    {
+        $this->cache = new CacheService();
+    }
+
+    public function dumps(): array
+    {
+        return $this->cache->cache('dump_count_by_region', function (): array {
+            $regions = $this->cache->regions();
+            $ids = $regions->pluck(['id']);
+            $allRegionIdsByDumps = Dump::with('region:id')->get(['id', 'cleared', 'dangerous']);
+
+            return $ids->map(function (int $id) use ($allRegionIdsByDumps, $regions): array {
+                $dumpsByRegion = $allRegionIdsByDumps->filter(fn($dump) => $dump->region->id === $id);
+                $total = $dumpsByRegion->count();
+                $cleared = $dumpsByRegion->filter(fn($dump) => $dump->cleared === true)->count();
+                $uncleared = $total - $cleared;
+                $region = $regions->find($id);
+                return [
+                    'id' => $id,
+                    'total' => $total,
+                    'dangerous' => $dumpsByRegion->filter(fn($regions) => $regions->dangerous === true && $regions->cleared === false)->count(),
+                    'cleared' => $cleared,
+                    'uncleared' => $uncleared,
+                    'area' => $region->area,
+                    'population' => $region->population,
+                ];
+            })->toArray();
+        });
+    }
+}
