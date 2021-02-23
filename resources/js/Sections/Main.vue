c8df20c67286e2ef291313dad2f9a39d56645cf2
Jure Rotar <jrotar0@gmail.com>
2021-02-23 12:51:31 +0100

    Added max width limitation
    Signed-off-by: Jure Rotar <jrotar0@gmail.com>

---
+++ resources/js/Sections/Main.vue
@@ -0,0 +1,25 @@
+<template>
+    <main :class="{'pointer-events-none': extended && width < breakpoints.md}"
+          class="flex justify-center p-4 lg:ml-72 lg:mt-16 dark:bg-dark-main duration-300 transition-colors">
+        <div class="max-w-main w-full">
+            <slot></slot>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "Main",
+    computed: {
+        width() {
+            return this.$store.state.width;
+        },
+        breakpoints() {
+            return this.$store.state.breakpoints;
+        },
+        extended() {
+            return this.$store.state.sidebarExtended;
+        }
+    },
+}
+</script>
