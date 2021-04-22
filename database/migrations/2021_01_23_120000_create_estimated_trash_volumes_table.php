1ffb02c870c38c12d77995c3c34d03b3db9abfdb
Jure Rotar <jrotar0@gmail.com>
2021-04-22 18:26:42 +0200

    Migration classes are now anonymous
    Signed-off-by: Jure Rotar <jrotar0@gmail.com>

--- database/migrations/2021_01_23_120000_create_estimated_trash_volumes_table.php
+++ database/migrations/2021_01_23_120000_create_estimated_trash_volumes_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEstimatedTrashVolumesTable extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -36,4 +35,4 @@ class CreateEstimatedTrashVolumesTable extends Migration
     {
         Schema::dropIfExists('estimated_trash_volumes');
     }
-}
+};
